e54d1a1de83d3b762ba9dd93f4aa71f1df0f7594
SongHB <s0h0b0@naver.com>
2025-05-26 21:45:36 +0900

    다시 에러 해결

--- Mission1/pipeline.groovy
+++ Mission1/pipeline.groovy
@@ -64,7 +64,7 @@ pipeline {
             
             emailtext(
 				subject: "Build 결과: ${currentBuild.currentResult}"
-				to: "shb9512@gmail.com, appleru1515@gmail.com, dive0dice@gmail.com"
+				to: "shb9512@gmail.com,appleru1515@gmail.com,dive0dice@gmail.com"
 				attachlog: true
 			)
         }
